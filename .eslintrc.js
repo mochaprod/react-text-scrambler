bb3582b3895441e5396bfdae73f0ec52bd151d00
Benjamin Huang <ben.huang06@gmail.com>
2018-02-04 14:40:50 -0500

    Dependencies and eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,39 @@
 module.exports = {
-    "extends": "google"
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "standard",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": "warn",
+        "space-before-function-paren": "off",
+        "operator-linebreak": "off"
+    }
 };
