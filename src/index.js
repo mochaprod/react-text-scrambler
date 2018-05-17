f3f8ab54abf9e4793df0ed8ea95425ba0837c5e3
Benjamin Huang <ben.huang06@gmail.com>
2018-05-16 23:58:04 -0400

    Add SimpleScrambler to exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Legacy components from 'Legacy' export.
 export { default as Legacy } from "./components/Legacy";
 
+export { default as SimpleScrambler } from "./components/SimpleScrambler";
 export { default as NewScrambler } from "./components/NewScrambler";
 export { default as TransitionScrambler } from "./components/TransitionScrambler";
+// export { default as Typewriter } from "./components/Typewriter";
 
 export { default as TextRenderer } from "./components/internals/TextRenderer";
